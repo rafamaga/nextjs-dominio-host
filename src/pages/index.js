3c3b8a2b0aff3e1d66a336fdad0ccea932c20e6b
Rafael Magalhães <rafaelvicente.magalhaes@gmail.com>
2024-08-03 16:36:53 -0300

    fear: alterar um pouco o código da tela inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
 export default function Home() {
+  const domain = window.location.hostname;
+  const host = window.location.host;
   return (
     <div>
       Hello World.{" "}
       <Link href="/about">
         About
       </Link>
+
+      <h1>Informações da Aplicação</h1>
+      <p><strong>Domínio:</strong> {domain}</p>
+      <p><strong>Host:</strong> {host}</p>
     </div>
   );
 }
